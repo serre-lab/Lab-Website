e251d72e45363962c42329d896f4e624406ccd36
monoguitari <jesus_rodriguez@brown.edu>
2025-05-20 13:44:17 -0400

    feat: people roles + uni + sort

--- src/pages/People/People.tsx
+++ src/pages/People/People.tsx
@@ -3,17 +3,34 @@ import Person from "../../components/Person/Person";
 import peopleData from "../../data/people.json";
 import "./People.css";
 
+// Define the desired role order
+const ROLE_ORDER = [
+  "PI",
+  "Assistant Prof of Research",
+  "PostDoc",
+  "Grad student",
+  "Research Assistant",
+  "Undergraduate Student",
+];
+
 export default function People() {
+  // Sort people by the custom role order
+  const sortedPeople = [...peopleData.people].sort(
+    (a, b) =>
+      ROLE_ORDER.indexOf(a.title) - ROLE_ORDER.indexOf(b.title)
+  );
+
   return (
     <div className="people-container">
       <Title order={1} className="people-page-title">Meet the Team</Title>
       <Divider my="sm" />
       <div className="people-grid">
-        {peopleData.people.map((person: any, index: number) => (
+        {sortedPeople.map((person: any, index: number) => (
           <Person
             key={index}
             fullName={person.fullName}
             title={person.title}
+            university={person.university}
             description={person.description}
             imagePath={person.imagePath}
           />
