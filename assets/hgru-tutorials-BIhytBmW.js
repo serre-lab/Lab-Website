19c40e62bd9661e8febc754322c6f662457f7089
monoguitari <jesus_rodriguez@brown.edu>
2025-06-14 18:42:25 -0500

    Updates

---
+++ assets/hgru-tutorials-BIhytBmW.js
@@ -0,0 +1,31 @@
+const e=`[< back to Resources](https://serre-lab.clps.brown.edu/resources/)
+
+# hGRU Tutorials
+
+- [Google Colab with code for hGRU on PathFinder-14](https://colab.research.google.com/drive/1nahqqz1xjVTgrhtPv6myE1xvi4eRvMge?authuser=1#scrollTo=aamsQuGRVUqn)
+- [Google Colab with Segmentation with hGRU-FPN on COCO](https://colab.research.google.com/drive/13q2pWR-32q6QfBRcz3fyihxGomZ3P4Ys?usp=sharing)
+
+---
+
+## Databases
+
+- [ClickMe](https://serre-lab.clps.brown.edu/resource/clickme/)
+- [The multi-cue boundary detection dataset](https://serre-lab.clps.brown.edu/resource/multicue/)
+- [HMDB: a large human motion database](https://serre-lab.clps.brown.edu/resource/hmdb-a-large-human-motion-database/)
+- [The Breakfast Actions Dataset](https://serre-lab.clps.brown.edu/resource/breakfast-actions-dataset/)
+
+## Software
+
+- [hGRU Tutorials](https://serre-lab.clps.brown.edu/resource/hgru-tutorials/)
+- [Lab GitHub repository](https://serre-lab.clps.brown.edu/resource/github/)
+- [Color processing](https://serre-lab.clps.brown.edu/resource/computational-mechanisms-of-color-processing-2/)
+- [Action recognition](https://serre-lab.clps.brown.edu/resource/action-recognition/)
+- [Automated system for rodent behavioral phenotyping](https://serre-lab.clps.brown.edu/resource/automated-home-cage-behavioral-phenotyping-of-mice/)
+- [Object recognition](https://serre-lab.clps.brown.edu/resource/object-recognition/)
+
+## Supplementary Information
+
+- [Learning sparse prototypes for crowd perception](https://serre-lab.clps.brown.edu/resource/zhangetalhbu2014/)
+- [Computational mechanisms of color processing](https://serre-lab.clps.brown.edu/resource/computational-mechanisms-of-color-processing/)
+- [A feedforward architecture accounts for rapid categorization](https://serre-lab.clps.brown.edu/resource/a-feedforward-architecture-accounts-for-rapid-categorization/)
+- [A neuromorphic approach to computer vision](https://serre-lab.clps.brown.edu/resource/a-neuromorphic-approach-to-computer-vision/)`;export{e as default};
---
+++ assets/neuroscience-BpBPcq1u.js
@@ -0,0 +1,43 @@
+const n=`[< back to Resources](https://serre-lab.clps.brown.edu/resources/)
+
+# Neuroscience
+
+## The Brain: An Introduction to Neuroscience (NEUR 0010, Fall, Paradiso & Stein)
+
+Provides a general introduction to basic topics in neuroscience, ranging from the special properties of the nerve cell to perceptual and motor systems, and higher order functions including language and motivated behavior.  
+*Graduate, Undergraduate*
+
+## Principles of Neurobiology (NEUR 1020, Spring, Connors)
+
+Covers fundamental concepts of molecular and cellular neurobiology including the structure of ion channels, synaptic transmission, synaptic development, molecular mechanisms of synaptic plasticity, learning and memory, and neurological diseases.  
+*Graduate, Undergraduate*
+
+## Advanced Cognitive Neuroscience (NEUR 2060, Spring, Sanes)
+
+Focuses on cognitive approaches to study nervous system function. Lectures and discussions focus on neurophysiology, neuroimaging and lesion analysis in mammals including humans. Computational approaches will become integrated into the material. Topics include the major cognitive systems, including perception, decisions, learning and memory, emotion and reward, language, and higher cortical function.  
+*Graduate, Undergraduate*
+
+## Neural Basis of Cognition (NEUR 1660, Spring, Mehta)
+
+Lecture course. Emphasizes the systems approach to neuroscience and examines several neural systems that mediate perception, action, higher visual and motor processing, learning, memory, attention, emotion, consciousness and sleep. The course focuses on experiments involving behavioral electrophysiology and discusses mechanisms mediating neural activity that mediates cognition.  
+*Graduate, Undergraduate*
+
+## Neural Systems (NEUR 1030, Fall, Sheinberg)
+
+Examines key principles of neural systems ranging in complexity from peripheral receptors to central control of behavior.  
+*Graduate, Undergraduate*
+
+## Advanced Systems Neuroscience (NEUR 2050, Fall, Sanes)
+
+Focuses on systems approaches to study nervous system function. Lectures and discussions focus on neurophysiology, neuroimaging and lesion analysis in mammals, including humans. Computational approaches will become integrated into the material. Topics include the major sensory, regulatory, and motor systems.  
+*Graduate, Undergraduate*
+
+## Computational Neuroscience (NEUR 1680, Spring, Bienenstock)
+
+A lecture and computing lab course providing an introduction to quantitative analysis of neural activity and encoding, as well as modeling of neurons and neural systems.  
+*Graduate, Undergraduate*
+
+## Mathematical Methods in the Brain Sciences (APMA 0410, Fall, Bienenstock)
+
+Basic mathematical methods commonly used in the cognitive and neural sciences. Topics include: introduction to differential equations, emphasizing qualitative behavior; introduction to probability and statistics, emphasizing hypothesis testing and modern nonparametric methods; and some elementary information theory. Examples from biology, psychology, and linguistics.  
+*Graduate, Undergraduate*`;export{n as default};
