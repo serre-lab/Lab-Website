b01bc592746e0ad5a424e076e7b743f883cd843d
monoguitari <jesus_rodriguez@brown.edu>
2025-06-14 11:36:17 -0500

    page styling consistent with mantine

--- src/components/MarkdownPage/MarkdownPage.tsx
+++ src/components/MarkdownPage/MarkdownPage.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+import { Title, Text, Anchor, List } from "@mantine/core";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import "./MarkdownPage.css";
@@ -6,10 +8,22 @@ interface MarkdownPageProps {
     content: string;
 }
 
+
 const MarkdownPage: React.FC<MarkdownPageProps> = ({ content }) => {
     return (
         <div className="markdown-container">
-            <ReactMarkdown>{content}</ReactMarkdown>
+            <ReactMarkdown
+                components={{
+                    h1: ({ node, ...props }) => <Title order={1} {...props} />,
+                    h2: ({ node, ...props }) => <Title order={2} {...props} />,
+                    p: ({ node, ...props }) => <Text {...props} />,
+                    a: ({ node, ...props }) => <Anchor {...props} />,
+                    ul: ({ node, ...props }) => <List {...props} />,
+                    // Add more mappings as needed
+                }}
+            >
+                {content}
+            </ReactMarkdown>
         </div>
     );
 };
