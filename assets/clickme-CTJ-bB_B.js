869445adbdb3d9d43ae9fa28840d75b56e0f802e
tserre <tserre@gmail.com>
2026-09-26 06:14:59 +0000

    Deploying to gh-pages from @ serre-lab/Lab-Website@9aa1eb70232a6bc127cabfadc23dbcc7b6eade27 🚀

---
+++ assets/clickme-CTJ-bB_B.js
@@ -0,0 +1,27 @@
+const e=`# ClickMe
+
+[**Play ClickMe Now and Win Cash Prizes! It’s a game that collects annotations on visual feature importance.**](https://clickme.clps.brown.edu)
+
+- [Links to original ClickMe datasets](https://connectomics.clps.brown.edu/tf_records/)
+- [Code for running GALA on TPUs](https://github.com/serre-lab/gala_tpu)
+
+---
+
+## Databases
+
+- [ClickMe](/resources/clickme)
+- [The multi-cue boundary detection dataset](/resources/the-multi-cue-boundary-detection-dataset)
+- [HMDB: a large human motion database](/hmdb51.html)
+- [The Breakfast Actions Dataset](/resources/the-breakfast-actions-dataset)
+
+## Software
+
+- [hGRU Tutorials](/resources/hgru-tutorials)
+- [Lab GitHub repository](https://github.com/serre-lab)
+- [Automated system for rodent behavioral phenotyping](/resources/automated-system-for-rodent-behavioral-phenotyping)
+
+## Supplementary Information
+
+- [Learning sparse prototypes for crowd perception](/resources/learning-sparse-prototypes-for-crowd-perception)
+- [Computational mechanisms of color processing](/resources/computational-mechanisms-of-color-processing)
+- [A neuromorphic approach to computer vision](/resources/a-neuromorphic-approach-to-computer-vision)`;export{e as default};
---
+++ assets/hgru-tutorials-eOOccyEG.js
@@ -0,0 +1,25 @@
+const o=`# hGRU Tutorials
+
+- [Google Colab with code for hGRU on PathFinder-14](https://colab.research.google.com/drive/1nahqqz1xjVTgrhtPv6myE1xvi4eRvMge?authuser=1#scrollTo=aamsQuGRVUqn)
+- [Google Colab with Segmentation with hGRU-FPN on COCO](https://colab.research.google.com/drive/13q2pWR-32q6QfBRcz3fyihxGomZ3P4Ys?usp=sharing)
+
+---
+
+## Databases
+
+- [ClickMe](/resources/clickme)
+- [The multi-cue boundary detection dataset](/resources/the-multi-cue-boundary-detection-dataset)
+- [HMDB: a large human motion database](/hmdb51.html)
+- [The Breakfast Actions Dataset](/resources/the-breakfast-actions-dataset)
+
+## Software
+
+- [hGRU Tutorials](/resources/hgru-tutorials)
+- [Lab GitHub repository](https://github.com/serre-lab)
+- [Automated system for rodent behavioral phenotyping](/resources/automated-system-for-rodent-behavioral-phenotyping)
+
+## Supplementary Information
+
+- [Learning sparse prototypes for crowd perception](/resources/learning-sparse-prototypes-for-crowd-perception)
+- [Computational mechanisms of color processing](/resources/computational-mechanisms-of-color-processing)
+- [A neuromorphic approach to computer vision](/resources/a-neuromorphic-approach-to-computer-vision)`;export{o as default};
